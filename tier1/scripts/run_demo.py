d955d92aa7367a42454bfa44ff1b3b1814896c2b
Gagan Khandate <gagankhandate@gmail.com>
2018-04-22 15:49:42 -0400

    updated run_demo tier1

--- tier1/scripts/run_demo.py
+++ tier1/scripts/run_demo.py
@@ -23,32 +23,31 @@ if __name__ == '__main__':
 	move_base = MoveBaseClient()
 
 	rospy.sleep(5)
-
-	while True:
 		
-		time =  tf_listener.getLatestCommonTime('/map','/base_link')
-		(trans,rot) = tf_listener.lookupTransform('/map','/base_link',time)
-		print(trans)
-		print(rot)
-		euler = tf.transformations.euler_from_quaternion(rot)
-		print(euler)
+	time =  tf_listener.getLatestCommonTime('/map','/base_link')
+	(trans,rot) = tf_listener.lookupTransform('/map','/base_link',time)
+	print(trans)
+	print(rot)
+	euler = tf.transformations.euler_from_quaternion(rot)
+	print(euler)
 
-		curr_pos = np.array([trans[0],trans[1],0])
+	curr_pos = np.array([trans[0],trans[1],0])
 
-		# goal_pos = curr_pos + np.random.random(3)*0.3 + np.array([0.1,0.1,0])
+	# goal_pos = curr_pos + np.random.random(3)*0.3 + np.array([0.1,0.1,0])
 
-		result = get_person_position('Thanos')
+	result = get_person_position('Thanos')
 
+	if result is not None:
 		d = np.linalg.norm(np.array([result.position.x - curr_pos[0],result.position.y - curr_pos[1]]))
-		d0 = 0.5
+		d0 = 0.8
 		if d < d0:
 			print("person too close")
 		else:
-			goal_pos_x = ((d-d0)/d)*(result.x - curr_pos[0]) + curr_pos[0]
-			goal_pos_y = ((d-d0)/d)*(result.y - curr_pos[1]) + curr_pos[1]
+			goal_pos_x = ((d-d0)/d)*(result.position.x - curr_pos[0]) + curr_pos[0]
+			goal_pos_y = ((d-d0)/d)*(result.position.y - curr_pos[1]) + curr_pos[1]
 			goal_pos_theta = np.arctan2(goal_pos_y - curr_pos[1],goal_pos_x - curr_pos[0])
 			print("moving to new goal")
-			print(goal_pos)
+			print([goal_pos_x,goal_pos_y,goal_pos_theta])
 			# move_base.goto(goal_pos[0],goal_pos[1],goal_pos[2])
 			move_base.goto(goal_pos_x,goal_pos_y,goal_pos_theta)
 
